c9a028b6aecdba129958f8401556fc0eff49a85a
Oussama Moutafatin <moutafatin3@gmail.com>
2023-04-08 16:56:29 +0000

    style guide

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif']
+      },
+      colors: {
+        primary: '#2F80ED'
+      }
+    }
   },
   plugins: [require('@tailwindcss/forms')]
 }
